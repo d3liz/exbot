9aed3babbacacf2bba83b7ca14f19f5ecae36eeb
d3liz <Nettylert@outlook.com>
2018-11-01 23:23:28 +0300

    fully working bot

--- exchanger.js
+++ exchanger.js
@@ -2,5 +2,5 @@ const Telegram = require('node-telegram-bot-api');
 const bot = new Telegram('635598975:AAHpVdDLbDg5_lOh3bwgTAhi3AkYi_XKySQ', {polling: true});
 bot.on( 'message', ctx => {
   console.log(ctx);
-  bot.sendMessage(ctx.chat.id,'Hello!');
+  bot.sendMessage( ctx.chat.id, JSON.stringify( bot.getMe() ) );
 } )
