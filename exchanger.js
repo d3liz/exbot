2c5cc05cc5052542c3d1cb437dec77be28e321d3
d3liz <Nettylert@outlook.com>
2018-11-01 23:46:33 +0300

    add some pretties to my bot

--- exchanger.js
+++ exchanger.js
@@ -3,6 +3,7 @@ const bot = new Telegram('635598975:AAHpVdDLbDg5_lOh3bwgTAhi3AkYi_XKySQ', {polli
 bot.on( 'message', ctx => {
   console.log(ctx);
   let name = (ctx.chat.last_name) ? ctx.chat.first_name + ' ' + ctx.chat.last_name : ctx.chat.first_name;
-  bot.sendPhoto(ctx.chat.id,'https://i0.wp.com/blog.yen.io/wp-content/uploads/2017/11/hello-world.png',{caption:'Hey, '+name+'. All working pretty good!'});
-  bot.getMe().then( inf => bot.sendMessage( ctx.chat.id, JSON.stringify( inf ) ) )
+  bot.getMe().then( inf => {
+    bot.sendPhoto(ctx.chat.id,'https://i0.wp.com/blog.yen.io/wp-content/uploads/2017/11/hello-world.png',{caption:'Hey, '+name+'. All working pretty good!'});
+    bot.sendMessage( ctx.chat.id, JSON.stringify( inf ) ) } )
 } )
