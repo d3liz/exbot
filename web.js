c30adfa11579d3b0bc0791fc88c0efd965e7a830
d3liz <Nettylert@outlook.com>
2018-11-01 22:56:54 +0300

    third try to deploy a simple bot on heroku

---
+++ web.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const packageInfo = require('./package.json');
+
+const app = express();
+
+app.get('/', ( req, res ) => {
+  res.json({
+    version: packageInfo.version,
+    alive: true });
+});
+
+const server = app.listen( process.env.PORT, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
